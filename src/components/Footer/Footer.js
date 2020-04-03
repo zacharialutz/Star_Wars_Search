5f6fcbd6f91e73fce346305b1449f6db4bc7bb55
Zacharia Lutz <zacharia.lutz@gmail.coom>
2020-04-02 22:45:00 -0500

    css variables and footer link update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
 	return (
 		<footer>
 			<p>Application &copy; 2020 Zacharia Lutz<br />Star Wars TM & &copy; Lucasfilm Ltd, All Rights Reserved</p>
 			<p>Created using the <a
-				href='https://swapi.co/'
+				href='https://swapi.co'
 				target='_blank'
 				rel='noopener noreferrer'
 			>Star Wars API</a> &copy; 2020 Paul Hallett</p>
